505ce02876fe69ca6b28a4c4b48f768ba91f8fea
Lucas Mussi <lucas-mussi@hotmail.com>
2023-05-20 14:38:44 -0300

    teste commit lucas 2

--- sistema_de_conformidade_etica/src/main/java/com/gestaoqualidadeprojetos/sistema_de_conformidade_etica/view/TelaTeste.java
+++ sistema_de_conformidade_etica/src/main/java/com/gestaoqualidadeprojetos/sistema_de_conformidade_etica/view/TelaTeste.java
@@ -45,7 +45,7 @@ public class TelaTeste extends javax.swing.JFrame {
 
         jTextArea1.setColumns(20);
         jTextArea1.setRows(5);
-        jTextArea1.setText("TESTE COMMIT LUCAS");
+        jTextArea1.setText("TESTE COMMIT LUCAS 2");
         jScrollPane1.setViewportView(jTextArea1);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
