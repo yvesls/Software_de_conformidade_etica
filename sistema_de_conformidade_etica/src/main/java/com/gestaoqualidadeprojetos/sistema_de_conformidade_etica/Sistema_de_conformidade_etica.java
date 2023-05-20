b7935ff9a8482daf3cd4fa84f7065bcf94700742
yvesls <yveslimasilva@gmail.com>
2023-05-20 12:51:32 -0300

    criando projeto maven

---
+++ sistema_de_conformidade_etica/src/main/java/com/gestaoqualidadeprojetos/sistema_de_conformidade_etica/Sistema_de_conformidade_etica.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.gestaoqualidadeprojetos.sistema_de_conformidade_etica;
+
+/**
+ *
+ * @author Note102
+ */
+public class Sistema_de_conformidade_etica {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
