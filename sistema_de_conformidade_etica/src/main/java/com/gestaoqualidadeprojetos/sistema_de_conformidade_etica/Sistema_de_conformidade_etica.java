e08e1f2b8c6e50fa1ad835e62de302f64a9a0a46
Lucas Mussi <lucas-mussi@hotmail.com>
2023-05-20 23:54:51 -0300

    telas criar user, logins, termo, tela inicial

--- sistema_de_conformidade_etica/src/main/java/com/gestaoqualidadeprojetos/sistema_de_conformidade_etica/Sistema_de_conformidade_etica.java
+++
@@ -1,8 +0,0 @@
-package com.gestaoqualidadeprojetos.sistema_de_conformidade_etica;
-
-public class Sistema_de_conformidade_etica {
-
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
-}
