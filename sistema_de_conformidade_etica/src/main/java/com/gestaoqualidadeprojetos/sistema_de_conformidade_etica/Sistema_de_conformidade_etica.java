f29d4dd954a111335b62f69b615b24a212be5b25
yvesls <yveslimasilva@gmail.com>
2023-05-22 10:26:49 -0300

    subindo telas dos casos: dashboard e questionário

--- sistema_de_conformidade_etica/src/main/java/com/gestaoqualidadeprojetos/sistema_de_conformidade_etica/Sistema_de_conformidade_etica.java
+++ sistema_de_conformidade_etica/src/main/java/com/gestaoqualidadeprojetos/sistema_de_conformidade_etica/Sistema_de_conformidade_etica.java
@@ -1,8 +1,12 @@
 package com.gestaoqualidadeprojetos.sistema_de_conformidade_etica;
 
+import com.gestaoqualidadeprojetos.sistema_de_conformidade_etica.view.QuestionarioView;
+import java.awt.EventQueue;
+
 public class Sistema_de_conformidade_etica {
 
+    
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        
     }
 }
