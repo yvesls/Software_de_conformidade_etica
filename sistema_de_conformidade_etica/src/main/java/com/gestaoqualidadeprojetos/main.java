eaec4e423b5533f83173982c87a3788d398ce805
yvesls <yveslimasilva@gmail.com>
2023-06-19 09:23:23 -0300

    configurando estrutura inicial

---
+++ sistema_de_conformidade_etica/src/main/java/com/gestaoqualidadeprojetos/main.java
@@ -0,0 +1,12 @@
+package com.gestaoqualidadeprojetos;
+
+
+/**
+ *
+ * @author Note102
+ */
+public class main {
+    public static void main(String args[]){
+        // chamar service aqui
+    }
+}
