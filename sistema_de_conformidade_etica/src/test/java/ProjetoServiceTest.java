ce98f9492bd418f5c334a13f5be43f8c11f4ef48
Lucas Mussi <lucas-mussi@hotmail.com>
2023-07-10 19:09:17 -0300

    remoção import

--- sistema_de_conformidade_etica/src/test/java/ProjetoServiceTest.java
+++ sistema_de_conformidade_etica/src/test/java/ProjetoServiceTest.java
@@ -13,7 +13,6 @@ import java.util.Date;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import org.junit.jupiter.api.Order;
 
 public class ProjetoServiceTest {
 
