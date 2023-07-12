a85f8f7498f72b3965b415cfc11bf2a4c2961856
gabriel <gabrielnama2@gmail.com>
2023-07-12 11:37:55 -0300

    Revert "Merge pull request #28 from yvesls/lucas"
    This reverts commit 777fdf627595e7dff0f0ce75ff8205b133d2c64c, reversing
    changes made to 282af404ccd3738a8800313ce6b145319059a2d3.

--- sistema_de_conformidade_etica/src/test/java/ProjetoServiceTest.java
+++ sistema_de_conformidade_etica/src/test/java/ProjetoServiceTest.java
@@ -13,6 +13,7 @@ import java.util.Date;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Order;
 
 public class ProjetoServiceTest {
 
